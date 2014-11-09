dda9d5fbfea3d2c48d0c5632843aaa4f1dff2cd0
Shreyas Palekar <shreyas.palekar@ufl.edu>
2014-11-09 12:11:52 -0500

    working parser and collector

---
+++ src/collector/twitterparams.py
@@ -0,0 +1,4 @@
+OAuthConsKey = 'N98MzS0vpqBKboydCAGTt3p7y'
+OAuthConsSecret = 'dwCCTwJDJ59cnDF4xi9HnJXmDhiZsILEaACi8aJCUB0ewpWcqK'
+OAuthToken = '219589742-6OOsWzpAwYgy2kvKTsKz0LS3re4wHZ5VOGDQWqlW'
+OAuthTokenSecret = 'PRJwNnbOJX3NypVX9XZvNt209eD2wDVfpDHyNCHYiNMxa'
